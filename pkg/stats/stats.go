da54b6edf372cfbdf441de6cb0aa8f478efd626d
Sadi <sadi.toirov2003@gmail.com>
2021-04-04 20:33:28 +0500

    new changes

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,13 +1,16 @@
 package stats
 
 import (
-	"github.com/ToirovSadi/bank/pkg/types"
+	"github.com/ToirovSadi/bank/v2/pkg/types"
 )
 
 func Avg(payment []types.Payment) types.Money {
 	var sum types.Money = 0
 	var n int = len(payment)
 	for _, payment := range payment {
+		if payment.Status == types.StatusFail {
+			continue
+		}
 		sum += payment.Amount
 	}
 	return sum / types.Money(n)
@@ -16,6 +19,9 @@ func Avg(payment []types.Payment) types.Money {
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	var sum types.Money = 0
 	for _, payment := range payments {
+		if payment.Status == types.StatusFail {
+			continue
+		}
 		if payment.Category == category {
 			sum += payment.Amount
 		}
