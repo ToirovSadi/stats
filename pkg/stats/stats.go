f26247c33f3fe9ec702f0f8db95f1fa5d041e52c
Sadi <sadi.toirov2003@gmail.com>
2021-04-05 00:22:31 +0500

    add func:res := PeriodsDynamic(map[types.Category]types.Money{

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -4,6 +4,29 @@ import (
 	"github.com/ToirovSadi/bank/v2/pkg/types"
 )
 
+func PeriodsDynamic(
+	first map[types.Category]types.Money, second map[types.Category]types.Money,
+) map[types.Category]types.Money {
+	// merge them(first, second)
+	for key := range first {
+		_, ok := second[key]
+		if !ok {
+			second[key] = 0
+		}
+	}
+	for key := range second {
+		_, ok := first[key]
+		if !ok {
+			first[key] = 0
+		}
+	}
+
+	for key := range second {
+		second[key] -= first[key]
+	}
+	return second
+}
+
 func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
 	res := map[types.Category]types.Money{}
 	cnt := map[types.Category]types.Money{}
